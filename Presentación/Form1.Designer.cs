425bb8d8dbb26c79d1e355fa434a714805a5c60b
Luca Guidotti <lucaguidotti93@gmail.com>
2022-05-24 11:10:02 -0300

    conexion Articulo a DB

---
+++ Presentación/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Presentación
+{
+    partial class frmCatalogo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvArticulos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvArticulos
+            // 
+            this.dgvArticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvArticulos.Location = new System.Drawing.Point(12, 57);
+            this.dgvArticulos.Name = "dgvArticulos";
+            this.dgvArticulos.Size = new System.Drawing.Size(695, 152);
+            this.dgvArticulos.TabIndex = 0;
+            // 
+            // frmCatalogo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 436);
+            this.Controls.Add(this.dgvArticulos);
+            this.Name = "frmCatalogo";
+            this.Text = "Catálogo";
+            this.Load += new System.EventHandler(this.frmCatalogo_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvArticulos;
+    }
+}
+
---
+++ Presentación/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentación
+{
+    public partial class frmCatalogo : Form
+    {
+        public frmCatalogo()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCatalogo_Load(object sender, EventArgs e)
+        {
+            CatalogoNegocio negocio = new CatalogoNegocio();
+            dgvArticulos.DataSource = negocio.Listar();
+        }
+    }
+}
